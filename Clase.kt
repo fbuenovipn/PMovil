bf5189d800973d093850ddcb982b08373804fdeb
Francisco Javier Bueno Vasquez <francisco.j.bueno@oracle.com>
2023-09-12 21:10:31 -0600

    1er Clase

---
+++ Clase.kt
@@ -0,0 +1,13 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+import kotlin.math.pow
+const val PI = 3.1416f
+const val PHI = 1.618f
+fun main(){
+
+}
+fun circleArea(radius:Float): Float{  
+    return PI*radius.pow(2)
+}
